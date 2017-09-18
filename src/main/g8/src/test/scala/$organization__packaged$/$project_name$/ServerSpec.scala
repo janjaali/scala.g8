38dc28a4eca80a0f064087c53e09144cc3c6cbf1
Siyavash Habashi <ghashange@live.com>
2017-09-18 16:14:08 +0200

    add directory structure

---
+++ src/main/g8/src/test/scala/$organization__packaged$/$project_name$/ServerSpec.scala
@@ -0,0 +1,19 @@
+package net.habashi.cicero
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{Matchers, WordSpec}
+
+class ServerSpec extends WordSpec with Matchers with ScalatestRouteTest with AnalysisResultJsonSupport {
+
+  // we have to call the main method manually to ensure all fields are initialized (see scala.App extends DelayedInit).
+  Server.main(null)
+
+  "The cicero server" should {
+    "return a message to signal the user that he maybe on the wrong path if he is not getting the evaluation path" in {
+      Get() ~> Server.route ~> check {
+        responseAs[String] shouldEqual "$project_name$"
+      }
+    }
+  }
+
+}
