546601123d3784f97b35d3db616cd2590a970678
Siyavash Habashi <ghashange@live.com>
2017-12-17 12:59:54 +0100

    add simple test

---
+++ src/main/g8/src/test/scala/$organization$/$project_name$Spec.scala
@@ -0,0 +1,11 @@
+package $organization$
+
+import org.scalatest.{FunSpec}
+
+class $project_name$ extends FunSpec {
+  describe("$project_name$") {
+    it("tests") {
+      assert(true)
+    }
+  }
+}
