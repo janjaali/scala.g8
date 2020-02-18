e43719a18b27e4232468eb7a6bc4b2b54eb84c3a
Siyavash Habashi <ghashange@live.com>
2020-02-18 21:54:11 +0100

    Replace LazyLogging for Logger(...).

--- src/main/g8/app/src/main/scala/$organization$/$project_name__space,Camel$.scala
+++ src/main/g8/app/src/main/scala/$organization$/$project_name__space,Camel$.scala
@@ -1,9 +1,13 @@
 package $organization$
 
-import com.typesafe.scalalogging.LazyLogging
+import com.typesafe.scalalogging.Logger
 
-object $project_name;format="Camel"$ extends LazyLogging {
+object $project_name;format="space,Camel"$ {
+
+  private val logger = Logger("$project_name;format="space,Camel"$")
+
   def main(args: Array[String]): Unit = {
+
     logger.info("$project_name$ started ...")
   }
 }
